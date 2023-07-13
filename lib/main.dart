553ece48450950cd638e19fe16c96e72247e9dbb
Seong-Hoon-Lim <hooney200@gmail.com>
2023-07-13 23:53:35 +0900

    ref - (권태뽕) Flutter + Firebase로 넷플릭스 UI 클론 코딩하기 [무작정 플러터]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix/screen/home_sereen.dart';
 import 'package:flutter_netflix/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -26,11 +27,7 @@ class _MyAppState extends State<MyApp> {
             body: TabBarView(
               physics: NeverScrollableScrollPhysics(),
               children: [
-                Container(
-                  child: Center(
-                    child: Text('home'),
-                  ),
-                ),
+                HomeScreen(),
                 Container(
                   child: Center(
                     child: Text('search'),
---
+++ lib/widget/carousel_slider.dart
@@ -0,0 +1,48 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import '../model/model_movie.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+  CarouselImage({required this.movies});
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  late List<Movie> movies;
+  late List<Widget> images;
+  late List<String> keywords;
+  late List<bool> likes;
+  late int _currentPage = 0;
+  late String _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = movies.map((m) => Image.asset(m.poster)).toList();
+    keywords = movies.map((m) => m.keyword).toList();
+    likes = movies.map((m) => m.like).toList();
+    _currentKeyword = keywords[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        Container(padding: EdgeInsets.all(20),
+        ),
+        CarouselSlider
+        (items: images,
+        onPageChanged: (index) {
+          setState(() {
+            _currentPage = index;
+            _currentKeyword = keywords[_currentPage];
+          });
+        }, options: null,
+      ],),
+    );
+  }
+
+
+}
