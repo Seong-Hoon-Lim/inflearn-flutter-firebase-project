4abecc83452843aac0ee85e6f302a21c449f19e0
Seong-Hoon-Lim <hooney200@gmail.com>
2023-07-14 01:18:06 +0900

    ref - (권태뽕) Flutter + Firebase로 넷플릭스 UI 클론 코딩하기 [무작정 플러터]

--- lib/screen/home_sereen.dart
+++ lib/screen/home_sereen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix/model/model_movie.dart';
+import 'package:flutter_netflix/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -7,13 +8,30 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
-    Movie.fromMap(
-        {
+    Movie.fromMap({
           'title': '사랑의 불시착',
           'keyword': '사랑/로맨스/판타지',
           'poster': 'test_movie_1.png',
           'like': false
-        })
+        }),
+    Movie.fromMap({
+          'title': '사랑의 불시착',
+          'keyword': '사랑/로맨스/판타지',
+          'poster': 'test_movie_1.png',
+          'like': false
+        }),
+    Movie.fromMap({
+          'title': '사랑의 불시착',
+          'keyword': '사랑/로맨스/판타지',
+          'poster': 'test_movie_1.png',
+          'like': false
+        }),
+    Movie.fromMap({
+          'title': '사랑의 불시착',
+          'keyword': '사랑/로맨스/판타지',
+          'poster': 'test_movie_1.png',
+          'like': false
+        }),
   ];
   @override
   void initState() {
@@ -22,7 +40,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(children: [
+      Stack(
+        children: [
+        CarouselImage(movies: movies),
+        TopBar(),
+      ],)
+    ],);
   }
 }
 
--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -4,7 +4,9 @@ import '../model/model_movie.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
+
   CarouselImage({required this.movies});
+
   _CarouselImageState createState() => _CarouselImageState();
 }
 
@@ -20,7 +22,7 @@ class _CarouselImageState extends State<CarouselImage> {
   void initState() {
     super.initState();
     movies = widget.movies;
-    images = movies.map((m) => Image.asset(m.poster)).toList();
+    images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
     keywords = movies.map((m) => m.keyword).toList();
     likes = movies.map((m) => m.like).toList();
     _currentKeyword = keywords[0];
@@ -29,20 +31,124 @@ class _CarouselImageState extends State<CarouselImage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [
-        Container(padding: EdgeInsets.all(20),
-        ),
-        CarouselSlider
-        (items: images,
-        onPageChanged: (index) {
-          setState(() {
-            _currentPage = index;
-            _currentKeyword = keywords[_currentPage];
-          });
-        }, options: null,
-      ],),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+          ),
+          CarouselSlider(
+              items: images,
+              options: CarouselOptions(onPageChanged: (index, reason) {
+                setState(() {
+                  _currentPage = index;
+                  _currentKeyword = keywords[_currentPage];
+                });
+              })),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      //likes[_currentPage] true 일 때 체크 버튼, false 일 때 플러스 버튼
+                      likes[_currentPage]
+                          ? IconButton(
+                              icon: Icon(Icons.check),
+                              onPressed: () {},
+                            )
+                          : IconButton(
+                              icon: Icon(Icons.add),
+                              onPressed: () {},
+                            ),
+                      Text(
+                        '내가 찜한 콘텐츠',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black,
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3),
+                        ),
+                        Text(
+                          '재생',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.info,
+                          color: Colors.white,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3),
+                        ),
+                        Text(
+                          '정보',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: makeIndicator(likes, _currentPage),
+            ),),
+        ],
+      ),
     );
   }
+}
 
-
+List<Widget> makeIndicator(List list, int _currentPage) {
+  List<Widget> results = [];
+  for (var i = 0; i < list.length; i++) {
+    results.add(Container(
+      width: 8,
+      height: 8,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: _currentPage == i
+            ? Color.fromRGBO(255, 255, 255, 0.9)
+            : Color.fromRGBO(255, 255, 255, 0.4),
+      ),
+    ));
+  }
+  return results;
 }
